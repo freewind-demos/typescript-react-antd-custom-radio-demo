cc895a5d5597ff8df94dc125444b5b49860340a6
Freewind <nowindlee@gmail.com>
2024-11-23 16:53:08 +0800

    显示勾

--- src/CustomRadio.tsx
+++ src/CustomRadio.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Radio } from 'antd';
 import type { RadioProps } from 'antd';
+import { CheckOutlined } from '@ant-design/icons';
+import './CustomRadio.css';
 
 export function CustomRadio(props: RadioProps) {
-  return <Radio {...props} />;
+  return (
+    <Radio {...props} className="custom-radio">
+      <span className="radio-content">
+        {props.children}
+      </span>
+    </Radio>
+  );
 }
